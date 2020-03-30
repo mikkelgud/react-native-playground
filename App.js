22dee49072212141539bb823f99ea07fde43d6bf
mikkelgud <mikkel_gudmundsen@hotmail.com>
2020-03-30 21:28:26 +0200

    Implemented handling of user input

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState(" ");
+  const [courseGoals, setCourseGoals] = ([]);
+
+  const enterGoalHandler = (enterGoal) =>{
+    setEnteredGoal(enterGoal);
+  };
+
+  const addGoalHandler = () =>{
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoal]);
+    // setCourseGoals([...courseGoals, enteredGoal]); Will also work, but maybe not always. 
+  };
+
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: "space-between", alignItems: "center"}}>
-        <TextInput 
-        placeholder="Write your goal here"
-        style={{ width: '80%' ,borderColor: 'black', borderWidth: 1, padding: 10 }}
+    <View style={styles.screen}>
+      <View style={styles.inputView}>
+        <TextInput
+          placeholder="Write your goal here"
+          style={styles.textInput}
+          onChangeText={enterGoalHandler}
+          value={enteredGoal}
         />
-        <Button title="+Add"/>
+        <Button 
+        title="+Add" 
+        style={styles.buttonStyle}
+        onPress={addGoalHandler}
+         />
       </View>
       <View>
       </View>
@@ -18,5 +37,22 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50,
+  },
+  inputView: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  textInput: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  buttonStyle: {
+    backgroundColor: 'blue',
+    width: 200
+  }
 });
