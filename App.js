ae1c3dbeef781e2d00017debbec06ae69c91618d
mikkelgud <mikkel_gudmundsen@hotmail.com>
2020-04-01 20:09:36 +0200

    Finished my goal list App. It is not about the app, it is about the content you fill it with that really matters

--- App.js
+++ App.js
@@ -1,33 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList  } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput'
 
 export default function App() {
 
   const [courseGoals, setCourseGoals] = useState([]);
-  
+  const [visibleState, setVisibleState] = useState(false);
+
 
   const addGoalHandler = enteredGoal => {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals, 
       { id: Math.random().toString(), value: enteredGoal}
     ]);
-    console.log("added " + enteredGoal )
+    console.log("added " + enteredGoal );
+    setVisibleState(false);
     // setCourseGoals([...courseGoals, enteredGoal]); Will also work, but maybe not always. 
   };
 
+  const removeGoalHandler = goalId =>{
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== goalId)
+    })
+  }
 
+  const cancelGoalHandler = () =>{
+    setVisibleState(false);
+  }
 
 
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}/>
+            <Button title="Add new goal" onPress={() => setVisibleState(true)}/> 
+      <GoalInput visible={visibleState} onAddGoal={addGoalHandler} onCancel={cancelGoalHandler} />
       <FlatList 
       keyExtractor={(item, index) => item.id}
       data={courseGoals} 
-      renderItem={itemData => <GoalItem title={itemData.item.value}/>}
+      renderItem={itemData =>   <GoalItem id={itemData.item.id} onPress={removeGoalHandler} title={itemData.item.value} />}
         />
     </View>
    );
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,48 +1,72 @@
 import React, {useState} from 'react';
-import {TextInput, View, StyleSheet, Button} from 'react-native';
+import {TextInput, View, StyleSheet, Button, Modal} from 'react-native';
 
 
 
 
 const GoalInput = props =>{
-  const [enteredGoal, setEnteredGoal] = useState(" ");
+  const [enteredGoal, setEnteredGoal] = useState("");
+
   
   const enterGoalHandler = (enterGoal) => {
     setEnteredGoal(enterGoal);
   };
+  
+  const addGoalHandler =()=>{
+    props.onAddGoal(enteredGoal);
+    setEnteredGoal(" ");
+  }
 
     return (
-    <View style={styles.inputView}>
+<Modal visible={props.visible} animationType="slide">   
+   <View style={styles.inputView}>
         <TextInput
-          placeholder="Write your goal here p"
+          placeholder="Write your goal here"
           style={styles.textInput}
           onChangeText={enterGoalHandler}
           value={enteredGoal}
         />
+        <View style={styles.buttonWrapper}>
+          <View style={styles.buttonStyle}>
         <Button
           title="+Add"
-          style={styles.buttonStyle}
-          onPress={props.onAddGoal.bind(this, enteredGoal)}
+          onPress={addGoalHandler}
         />
+        </View>
+        <View style={styles.buttonStyle}>
+         <Button
+          title="cancel"
+          color="red"
+          onPress={props.onCancel}
+        /></View>
       </View>
+      </View>
+      </Modal>
+
       )
 }
 
 const styles = StyleSheet.create({
-  inputView :{
-    flexDirection: 'row',
-    justifyContent: "space-between",
+  inputView: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center"
   },
   buttonStyle:{
-    backgroundColor: 'blue',
-    width: 200
+    width: '45%'
   },
   textInput: {
     width: '80%',
     borderColor: 'black',
     borderWidth: 1,
-    padding: 10
+    padding: 10,
+    marginBottom: 20,
+    color: 'black'
+  },
+  buttonWrapper:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: '50%'
   },
 })
 
